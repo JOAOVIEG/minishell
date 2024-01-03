435973ede9de57eba6ee1b3e92e2c8911bb899b5
William Ferreira Rocha <wiferrei@c2r9s1.42lisboa.com>
2024-01-03 11:05:13 +0000

    makefile is done

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,12 +6,18 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:22:51 by wiferrei          #+#    #+#             */
-/*   Updated: 2024/01/03 10:42:51 by wiferrei         ###   ########.fr       */
+/*   Updated: 2024/01/03 11:04:26 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 #include "./libft/includes/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
 
 #endif
