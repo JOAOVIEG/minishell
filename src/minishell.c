4b03f951c73ee612b178fb9b20f61617ce800e32
William Ferreira Rocha <wiferrei@c2r10s1.42lisboa.com>
2024-01-16 11:52:20 +0000

    changed the read line function

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:27:24 by wiferrei          #+#    #+#             */
-/*   Updated: 2024/01/12 18:58:57 by wiferrei         ###   ########.fr       */
+/*   Updated: 2024/01/16 11:50:55 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*read_input(void)
 {
 	char	*line;
 
-	line = readline("");
+	line = readline("Minishell$ ");
 	if (line && *line)
 		add_history(line);
 	return (line);
@@ -26,27 +26,24 @@ int	main(int argc, char **argv, char **envp)
 {
 	char		*line;
 	int			status;
-	t_prompt	*shell_prompt;
+	
 
 	(void)argc;
 	(void)argv;
 	(void)envp;
 	status = 1;
-	shell_prompt = set_prompt("minishell$ ");
 	while (status)
 	{
-		printf("%s", shell_prompt->prompt_name);
 		line = read_input();
 		   if (!line)
         {
             // Handle Ctrl+D
-            free(shell_prompt);
+           
             exit(EXIT_SUCCESS);
         }
 		if (ft_strcmp(line, "exit") == 0)
 		{
 			free(line);
-			free(shell_prompt);
 			exit(EXIT_SUCCESS);
 		}
 		if (ft_strcmp(line, "pwd") == 0)
