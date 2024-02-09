d665f65570bc8b18952c7cb6808c927c1c69b8d1
William Rocha <william@pop-os.localdomain>
2024-02-09 16:06:47 +0000

    ok

--- includes/parser.h
+++ includes/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joaocard <joaocard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:08:02 by wiferrei          #+#    #+#             */
-/*   Updated: 2024/02/09 15:33:56 by joaocard         ###   ########.fr       */
+/*   Updated: 2024/02/09 16:04:02 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,6 @@ void					tree_simple_command(t_parser *parser);
 
 // free_parser
 void					reset_parser(t_parser *parser);
-void					free_parser(t_parser *parser);
+
 
 #endif
