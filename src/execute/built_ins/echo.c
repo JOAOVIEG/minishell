e918d3771183dc20f7e979ddb32c78966fdeee02
joaovieg <joao.c.90.viegas@gmail.com>
2024-03-02 15:46:20 +0000

    echo builtin bug is fixed

--- src/execute/built_ins/echo.c
+++ src/execute/built_ins/echo.c
@@ -6,7 +6,7 @@
 /*   By: joaocard <joaocard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 15:02:43 by joaocard          #+#    #+#             */
-/*   Updated: 2024/02/28 15:02:35 by joaocard         ###   ########.fr       */
+/*   Updated: 2024/03/02 15:43:46 by joaocard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,82 @@
 
 int	echo(char **cmd)
 {
-	int	newline;
-	int	i;
+    int	i;
+	int n_flag;
+	
+	n_flag = flag_handle(cmd);
+	i = to_print(cmd);
+    while (cmd[i])
+    {
+        if ((printf("%s", cmd[i])) < 0)
+            return ((shell()->status = printf("%s", cmd[i])));
+        if (cmd[i + 1])
+        {
+            if ((printf(" ")) < 0)
+                return (shell()->status = printf(" "));
+        }
+        i++;
+    }
+    if (n_flag)
+    {
+        if ((printf("\n")) < 0)
+            return ((shell()->status = printf("\n")));
+    }
+    shell()->status = 0;
+    return (shell()->status);
+}
 
-	newline = 1;
+int	flag_handle(char **cmd)
+{
+	int i;
+	int	j;
+	int	n_flag;
+	
 	i = 1;
+	n_flag = 1;
+	while (cmd[i])
+    {
+        if (cmd[i][0] == '-' && cmd[i][1] == 'n')
+        {
+            j = 2;
+            while (cmd[i][j] == 'n')
+                j++;
+            if (cmd[i][j] == '\0')
+            {
+                n_flag = 0;
+                i++;
+            }
+            else
+                break ;
+        }
+        else
+            break ;
+    }
+	return (n_flag);
+}
+
+int	to_print(char **cmd)
+{
+	int i;
+	int	j;
+	
 	i = 1;
-	/*todo @joao -nnnnn -n -n -nn -n*/
-	if (cmd[i] && ft_strcmp(cmd[i], "-n") == 0)
-	{
-		newline = 0;
-		i++;
-	}
-	while (cmd && cmd[i])
-	{
-		if ((printf("%s", cmd[i])) < 0)
-			return ((shell()->status = printf("%s", cmd[i])));
-		if (cmd[i + 1] && (cmd + i + 1))
-		{
-			if ((printf(" ")) < 0)
-				return (shell()->status = printf(" "));
-		}
-		i++;
-	}
-	if (newline)
-	{
-		if ((printf("\n")) < 0)
-			return ((shell()->status = printf("\n")));
-	}
-	shell()->status = 0;
-	return (shell()->status);
+	while (cmd[i])
+    {
+        if (cmd[i][0] == '-' && cmd[i][1] == 'n')
+        {
+            j = 2;
+            while (cmd[i][j] == 'n')
+                j++;
+            if (cmd[i][j] == '\0')
+            {
+                i++;
+            }
+            else
+                break ;
+        }
+        else
+            break ;
+    }
+	return (i);
 }
