54f42bcc0b29ddf74e78d7504934105b9f226ddb
William Rocha <william@pop-os.localdomain>
2024-03-05 16:30:32 +0000

    heredoc and append is done

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 16:08:02 by wiferrei          #+#    #+#             */
-/*   Updated: 2024/03/05 14:55:24 by wiferrei         ###   ########.fr       */
+/*   Updated: 2024/03/05 15:32:33 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,9 @@ typedef struct s_token_queue
 
 typedef struct s_buffer
 {
-	t_lst_tokens		*lst_tok;
-	t_lst_tokens		*next;
+	t_token_queue	cmds;
+	t_token_queue	heredocs;
+	t_token_queue	redir_files;
 }						t_buffer;
 
 typedef struct s_parser
@@ -59,7 +60,6 @@ typedef struct s_parser
 	int					pipe_count;
 	int					redir_count;
 	int					heredoc_count;
-	t_buffer			*buffer;
 
 }						t_parser;
 
--- src/parser/ast_redir.c
+++ src/parser/ast_redir.c
@@ -6,7 +6,7 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 17:23:16 by wiferrei          #+#    #+#             */
-/*   Updated: 2024/03/05 15:23:07 by wiferrei         ###   ########.fr       */
+/*   Updated: 2024/03/05 16:21:38 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@ void	init_token_queue(t_token_queue *queue)
 {
 	queue->head = NULL;
 	queue->tail = NULL;
+}
+
+void	init_buffer(t_buffer *buffer)
+{
+	init_token_queue(&buffer->cmds);
+	init_token_queue(&buffer->heredocs);
+	init_token_queue(&buffer->redir_files);
 }
 
 void	clean_token_queue(t_token_queue *queue)
@@ -33,6 +40,13 @@ void	clean_token_queue(t_token_queue *queue)
 	}
 	queue->head = NULL;
 	queue->tail = NULL;
+}
+
+void	clean_buffer(t_buffer *buffer)
+{
+	clean_token_queue(&buffer->cmds);
+	clean_token_queue(&buffer->heredocs);
+	clean_token_queue(&buffer->redir_files);
 }
 
 void	add_token_back(t_lst_tokens **current, t_lst_tokens **cmd_tokens,
@@ -77,6 +91,8 @@ char	**get_redir_files(t_lst_tokens *tokens)
 
 	i = 0;
 	current = tokens;
+	if (!current)
+		return (NULL);
 	file = (char **)ft_calloc_memory(lst_token_size(tokens) + 1,
 			sizeof(char *));
 	while (current != NULL)
@@ -89,16 +105,18 @@ char	**get_redir_files(t_lst_tokens *tokens)
 	return (file);
 }
 
-t_node	*new_redir_tree_node(t_token_queue cmds, t_token_queue redir_files)
+t_node	*new_redir_tree_node(t_buffer buffer)
 {
 	t_node	*new_node;
 
-	new_node = new_tree_node(cmds.head);
-	new_node->cmd->file = get_redir_files(redir_files.head);
+	new_node = new_tree_node(buffer.cmds.head);
+	new_node->cmd->file = get_redir_files(buffer.redir_files.head);
+	new_node->cmd->heredoc = get_redir_files(buffer.heredocs.head);
 	return (new_node);
 }
 
-t_node	*new_heredoc_node(t_token_queue cmds, t_token_queue heredocs, t_token_queue redir_files)
+t_node	*new_heredoc_node(t_token_queue cmds, t_token_queue heredocs,
+		t_token_queue redir_files)
 {
 	t_node	*new_node;
 
@@ -108,43 +126,43 @@ t_node	*new_heredoc_node(t_token_queue cmds, t_token_queue heredocs, t_token_que
 	return (new_node);
 }
 
-void	build_token_queue(t_lst_tokens **current, t_token_queue *cmds,
-		t_token_queue *redir_files)
+void	build_token_queue(t_lst_tokens **current, t_buffer *buffer)
 {
 	while (*current != NULL && (*current)->type != TYPE_PIPE)
 	{
-		if ((*current)->type == TYPE_REDIRECT)
-			redir_files->head = get_redir_list(current, &redir_files->head,
-					&redir_files->tail);
+		if ((*current)->type == TYPE_HEREDOC)
+			buffer->heredocs.head = get_redir_list(current,
+					&buffer->heredocs.head, &buffer->heredocs.tail);
+		else if ((*current)->type == TYPE_REDIRECT)
+			buffer->redir_files.head = get_redir_list(current,
+					&buffer->redir_files.head, &buffer->redir_files.tail);
 		else
-			cmds->head = get_cmd_list(current, &cmds->head, &cmds->tail);
+			buffer->cmds.head = get_cmd_list(current, &buffer->cmds.head,
+					&buffer->cmds.tail);
 	}
 }
 
 void	build_redir_tree(t_shell *shell)
 {
-	t_token_queue	cmds;
-	t_token_queue	redir_files;
+	t_buffer		buffer;
 	t_lst_tokens	*current;
 	t_node			*tree;
 
-	init_token_queue(&cmds);
-	init_token_queue(&redir_files);
+	init_buffer(&buffer);
 	current = shell->parser->tokens;
 	while (current != NULL)
-		build_token_queue(&current, &cmds, &redir_files);
-	tree = new_redir_tree_node(cmds, redir_files);
-	clean_token_queue(&cmds);
-	clean_token_queue(&redir_files);
+		build_token_queue(&current, &buffer);
+	tree = new_redir_tree_node(buffer);
+	clean_buffer(&buffer);
 	shell->node = tree;
 }
 
 t_node	*create_node_and_update_redir_pipe_tree(t_node **tree_root,
-		t_node **rightmost_node, t_token_queue cmds, t_token_queue redir_files)
+		t_node **rightmost_node, t_buffer buffer)
 {
 	t_node	*new_node;
 
-	new_node = new_redir_tree_node(cmds, redir_files);
+	new_node = new_redir_tree_node(buffer);
 	if (!new_node)
 		return (NULL);
 	if (!*tree_root)
@@ -158,47 +176,27 @@ t_node	*create_node_and_update_redir_pipe_tree(t_node **tree_root,
 	return (new_node);
 }
 
-t_node	*create_node_and_update_heredoc_pipe_tree(t_node **tree_root,
-		t_node **rightmost_node, t_token_queue cmds, t_token_queue heredocs, t_token_queue redir_files)
-{
-	t_node	*new_node;
-	
-	new_node = new_heredoc_node(cmds, heredocs, redir_files);
-	if (!new_node)
-		return (NULL);
-	if (!*tree_root)
-	{
-		*tree_root = new_node;
-		*rightmost_node = new_node;
-	}
-	else
-		update_tree_root(tree_root, rightmost_node,
-			create_pipe_node(*rightmost_node, new_node));
-	return (new_node);
-}
+
 
 void	build_redir_pipe_tree(t_shell *shell)
 {
 	t_node			*tree_root;
 	t_node			*rightmost_node;
-	t_token_queue	cmds;
-	t_token_queue	redir_files;
+	t_buffer		buffer;
 	t_lst_tokens	*current;
 
 	tree_root = NULL;
 	rightmost_node = NULL;
-	init_token_queue(&cmds);
-	init_token_queue(&redir_files);
+	init_buffer(&buffer);
 	current = shell->parser->tokens;
 	while (current != NULL)
 	{
-		build_token_queue(&current, &cmds, &redir_files);
+		build_token_queue(&current, &buffer);
 		rightmost_node = create_node_and_update_redir_pipe_tree(&tree_root,
-				&rightmost_node, cmds, redir_files);
+				&rightmost_node, buffer);
 		if (current != NULL)
 			current = current->next;
-		clean_token_queue(&cmds);
-		clean_token_queue(&redir_files);
+		clean_buffer(&buffer);
 	}
 	shell->node = tree_root;
 }
@@ -216,7 +214,7 @@ void	build_heredoc_queue(t_lst_tokens **current, t_token_queue *cmds,
 	}
 }
 
-void build_heredoc_redir_queue(t_lst_tokens **current, t_token_queue *cmds,
+void	build_heredoc_redir_queue(t_lst_tokens **current, t_token_queue *cmds,
 		t_token_queue *heredocs, t_token_queue *redir_files)
 {
 	while (*current != NULL && (*current)->type != TYPE_PIPE)
@@ -230,32 +228,25 @@ void build_heredoc_redir_queue(t_lst_tokens **current, t_token_queue *cmds,
 		else
 			cmds->head = get_cmd_list(current, &cmds->head, &cmds->tail);
 	}
-
 }
-
 
 void	build_heredoc_tree(t_shell *shell)
 {
 	t_node			*tree;
 	t_lst_tokens	*current;
-	t_token_queue	cmds;
-	t_token_queue	heredocs;
-	t_token_queue	redir_files;
+	t_buffer		buffer;
 
 	tree = NULL;
-	init_token_queue(&cmds);
-	init_token_queue(&heredocs);
-	init_token_queue(&redir_files);
+	init_buffer(&buffer);
 	current = shell->parser->tokens;
 	while (current != NULL)
 	{
-		build_heredoc_queue(&current, &cmds, &heredocs);
+		build_token_queue(&current, &buffer);
 		if (current != NULL)
 			current = current->next;
 	}
-	tree = new_heredoc_node(cmds, heredocs, redir_files);
-	clean_token_queue(&cmds);
-	clean_token_queue(&heredocs);
+	tree = new_redir_tree_node(buffer);
+	clean_buffer(&buffer);
 	shell->node = tree;
 }
 
@@ -263,56 +254,68 @@ void	build_heredoc_pipe_tree(t_shell *shell)
 {
 	t_node			*tree_root;
 	t_node			*rightmost_node;
-	t_token_queue	cmds;
-	t_token_queue	heredocs;
-	t_token_queue	redir_files;
+	t_buffer		buffer;
 	t_lst_tokens	*current;
 
 	tree_root = NULL;
 	rightmost_node = NULL;
-	init_token_queue(&cmds);
-	init_token_queue(&heredocs);
-	init_token_queue(&redir_files);
+	init_buffer(&buffer);
 	current = shell->parser->tokens;
 	while (current != NULL)
 	{
-		build_heredoc_queue(&current, &cmds, &heredocs);
-		rightmost_node = create_node_and_update_heredoc_pipe_tree(&tree_root,
-				&rightmost_node, cmds, heredocs, redir_files);
+		build_token_queue(&current, &buffer);
+		rightmost_node = create_node_and_update_redir_pipe_tree(&tree_root,
+				&rightmost_node, buffer);
 		if (current != NULL)
 			current = current->next;
-		clean_token_queue(&cmds);
-		clean_token_queue(&heredocs);
+		clean_buffer(&buffer);
 	}
 	shell->node = tree_root;
 }
-
 
 void	build_heredoc_redir_tree(t_shell *shell)
 {
 	t_node			*tree_root;
 	t_node			*rightmost_node;
-	t_token_queue	cmds;
-	t_token_queue	heredocs;
-	t_token_queue	redir_files;
+	t_buffer		buffer;
 	t_lst_tokens	*current;
 
 	tree_root = NULL;
 	rightmost_node = NULL;
-	init_token_queue(&cmds);
-	init_token_queue(&heredocs);
-	init_token_queue(&redir_files);
+	init_buffer(&buffer);
 	current = shell->parser->tokens;
 	while (current != NULL)
 	{
-		build_heredoc_redir_queue(&current, &cmds, &heredocs, &redir_files);
-		rightmost_node = create_node_and_update_heredoc_pipe_tree(&tree_root,
-				&rightmost_node, cmds, heredocs, redir_files);
+		build_token_queue(&current, &buffer);
+		rightmost_node = create_node_and_update_redir_pipe_tree(&tree_root,
+				&rightmost_node, buffer);
 		if (current != NULL)
 			current = current->next;
-		clean_token_queue(&cmds);
-		clean_token_queue(&heredocs);
-		clean_token_queue(&redir_files);
+		clean_buffer(&buffer);
+	}
+	shell->node = tree_root;
+}
+
+void build_heredoc_redir_pipe_tree(t_shell *shell)
+{
+	t_node			*tree_root;
+	t_node			*rightmost_node;
+	t_buffer		buffer;
+	t_lst_tokens	*current;
+
+	tree_root = NULL;
+	rightmost_node = NULL;
+	init_buffer(&buffer);
+	current = shell->parser->tokens;
+	while (current != NULL)
+	{
+		build_heredoc_redir_queue(&current, &buffer.cmds, &buffer.heredocs,
+				&buffer.redir_files);
+		rightmost_node = create_node_and_update_redir_pipe_tree(&tree_root,
+				&rightmost_node, buffer);
+		if (current != NULL)
+			current = current->next;
+		clean_buffer(&buffer);
 	}
 	shell->node = tree_root;
 }
