6910e91ae66f4d176eb29d8bbd6cb2a887e2b03f
William Rocha <william@pop-os.localdomain>
2024-03-16 18:38:29 +0000

    f+++

--- src/execute/built_ins/export.c
+++ src/execute/built_ins/export.c
@@ -6,85 +6,128 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:26:20 by joaocard          #+#    #+#             */
-/*   Updated: 2024/03/15 20:08:47 by wiferrei         ###   ########.fr       */
+/*   Updated: 2024/03/16 18:37:59 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
+// void	export(char **arg)
+// {
+// 	t_env	*env;
+// 	t_env	*new;
+// 	char	*name;
+// 	char	*value;
+// 	char	*equal;
+// 	int		i;
+
+// 	i = 1;
+// 	env = shell()->v_env;
+// 	if (!arg[i])
+// 		display_exp_var(env);
+// 	else
+// 	{
+// 		while (arg[i])
+// 		{
+// 			new = NULL;
+// 			equal = get_equal(arg[i]);
+// 			if (equal)
+// 			{
+// 				name = get_var_name(arg[i], equal);
+// 				value = ft_strdup(equal + 1);
+// 			}
+// 			else
+// 			{
+// 				name = ft_strdup(arg[i]);
+// 				value = get_var_value(env, name);
+// 			}
+// 			if (is_invalid_variable(name))
+// 			{
+// 				printf("minishell: export: `%s': not a valid identifier\n",
+// 					name);
+// 				shell()->status = STDERR_FILENO;
+// 				free(name);
+// 				free(value);
+// 				return ;
+// 			}
+// 			shell()->v_env = update_envl(env, new, name, value);
+// 			i++;
+// 		}
+// 	}
+// 	shell()->status = EXIT_SUCCESS;
+// }
+
+
 void	export(char **arg)
 {
 	t_env	*env;
-	t_env	*new;
-	char	*name;
-	char	*value;
-	char	*equal;
-	int		i;
+	//t_env	*new;
+	// char	*name;
+	// char	*value;
+	// char	*equal;
+	t_lexer	*lexer;
 
-	i = 1;
 	env = shell()->v_env;
-	if (!arg[i])
+	if (!arg[1])
 		display_exp_var(env);
 	else
 	{
-		while (arg[i])
+		lexer = init_lexer();
+		lexer->input = strdup(arg[1]);
+		printf("lexer->input = %s\n", lexer->input);
+		lexer->input_size = ft_strlen(lexer->input);
+		lexer->tokens = split_into_tokens(lexer);
+		for (int i = 0; lexer->tokens[i] != NULL; i++)
 		{
-			new = NULL;
-			equal = get_equal(arg[i]);
-			if (equal)
-			{
-				name = get_var_name(arg[i], equal);
-				value = ft_strdup(equal + 1);
-			}
-			else
-			{
-				name = ft_strdup(arg[i]);
-				value = get_var_value(env, name);
-			}
-			if (is_invalid_variable(name))
-			{
-				printf("minishell: export: `%s': not a valid identifier\n",
-					name);
-				shell()->status = STDERR_FILENO;
-				free(name);
-				free(value);
-				return ;
-			}
-			shell()->v_env = update_envl(env, new, name, value);
-			i++;
+			printf("lexer->tokens[%d] = %s\n", i, lexer->tokens[i]);
 		}
+		//shell()->v_env = update_envl(env, new, name, value);
 	}
 	shell()->status = EXIT_SUCCESS;
 }
 
 t_env	*create_var(t_env *new, char *name, char *value)
 {
+	new = (t_env *)malloc(sizeof(t_env));
+	if (new == NULL)
+		return (NULL);
 	new->name = name;
 	new->value = value;
 	new->next = shell()->v_env;
-	shell()->v_env = new->next;
+	shell()->v_env = new;
 	return (new);
 }
 
 char	*get_var_name(char *arg, char *equal)
 {
 	char	*name;
+	int		length;
 
-	name = malloc(sizeof(char) * ((equal - arg) + 1));
-	ft_strncpy(name, arg, equal - arg);
-	name[equal - arg] = '\0';
+	length = 0;
+	while (&arg[length] != equal)
+		length++;
+	name = malloc(sizeof(char) * (length + 1));
+	ft_strncpy(name, arg, length);
+	name[length] = '\0';
 	return (name);
 }
 
 t_env	*update_envl(t_env *env, t_env *new, char *name, char *value)
 {
+	t_env	*head;
+
+	head = env;
 	while (env && ft_strcmp(env->name, name) != 0)
 		env = env->next;
 	if (env)
 		env->value = ft_strdup(value);
-	else if (!value)
-		create_var(new, name, value);
-	return (env);
+	else if (value)
+	{
+		new = create_var(new, name, value);
+		new->next = head;
+		head = new;
+	}
+	return (head);
 }
 
 char	*get_equal(char *arg)
